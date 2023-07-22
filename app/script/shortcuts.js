60e27d6fcf04cb2d72b42678699239de4c40f7fa
Max <max@maximilian.co.nz>
2023-07-22 14:05:21 +1200

    Reworked sidebar to shift page content

--- app/script/shortcuts.js
+++ app/script/shortcuts.js
@@ -4,21 +4,7 @@ document.addEventListener("keydown", (e) => {
 	// Toggle the sidebar
 	if (e.ctrlKey && e.key === "b") {
 
-		console.log("Toggling sidebar rn");
-		const sidebar = document.querySelector(".sidebar");
-
-		// Check for if the sidebar is hidden, or not then swap the state
-		if (sidebar.classList.contains("show")) {
-			
-			// Hide the sidebar
-			sidebar.classList.remove("show");
-			sidebar.classList.add("hide");
-		} else {
-			
-			// Show the sidebar
-			sidebar.classList.remove("hide");
-			sidebar.classList.add("show");
-		}
+		document.querySelector(".sidebar").classList.toggle("hidden");
 	}
 
 });
