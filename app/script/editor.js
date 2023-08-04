fb1ecdbc430c4becb3b9d2950fd05e470061d617
Max <max@maximilian.co.nz>
2023-08-05 11:51:29 +1200

    new lines can kinda be added idk. reworking to use array of lines instead of single string.

--- app/script/editor.js
+++ app/script/editor.js
@@ -1,10 +1,13 @@
-const editor = document.querySelector("#editor");
-let output = "";
+const statusBar = document.querySelector(".status-bar");
+const editor = document.querySelector(".textarea");
+
+//? Each item in the array is a new line
+let output = [""];
 
 // Store the cursor position rows and columns
 // TODO: Smooth cursor animation
-let cursorPosition = [1, 1];
-const cursor = `<span class="cursor">|</span>`;	
+let caretX = 0;
+let caretY = 0;
 
 window.addEventListener("keydown", (e) => {
 
@@ -13,27 +16,74 @@ window.addEventListener("keydown", (e) => {
 
 	// Check for if they want to do an action like delete or new line
 	switch (e.key) {
+
 		case "Backspace":
 			output = output.slice(0, -1);
-			cursorPosition[0]--;
+			caretX--;
+
+			// Check if we are going up a line
+			if (caretX <= 1)
+			{
+				// Decrease the Y and reset the X
+				caretY--;
+				caretX = 0;
+			}
 			break;
-	
+
 		case "Enter":
-			output += "\n";
-			cursorPosition[1]++;
+
+			// Increase the Y and reset the X
+			caretY++;
+			caretX = 0;
+
+			// Make the new line
+			if (!output[caretY]) output[caretY] = "";
 			break;
+
 	}
+
+
 
 	// Check for if the key string is larger than 1 character
+
 	if (e.key.length === 1) {
-		output += e.key;
-		cursorPosition[0]++;
+
+		output[caretY] += e.key;
+		caretX++;
 	}
 
-	// TODO: Don't replace all the time. Edit instead
+	// Clamp cursor position
+	if (caretX < 0) caretX = 0;
+	if (caretY < 0) caretY = 0;
 
-	// Add the cursor and make it html
-	const outputCursor = output.slice(0, cursorPosition[0]) + cursor + output.slice(cursorPosition[0]);
-	const outputHtml = `<p>${outputCursor}</p>`;
+
+	// TODO: Don't replace the content all the time. Edit instead
+	// TODO: Don't replace the caret. Use the old one
+
+	// Add the cursor to the output html
+	// TODO: Add smooth cursor animation and blinking animation
+	const caret = `<div class="caret"></div>`;
+	let outputHtml = ``;
+	for (let i = 0; i < output.length; i++) {
+
+		// Check if the cursor should be added
+		if (i == caretY) outputHtml += `<pre>${output[i].slice(0, caretX)}${caret}${output[i].slice(caretX)}</pre>`;
+		else outputHtml += output[i];
+
+		// Check if a new line must be added
+		if (i != output.length) outputHtml += "\n";
+	}
+
+
+	// Write the text to the DOM
 	editor.innerHTML = outputHtml;
+
+
+
+	// TODO: Row reset to 0 when go to next column
+
+
+
+	//! Stringify to see escaped characters btw.
+	console.log(`${JSON.stringify(output)}\n${[caretX, caretY]}\n${output}\n`);
 });
