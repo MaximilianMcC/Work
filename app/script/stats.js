c1c583483f716a1f69ce457d81e5882b2e1b09dd
Max <max@maximilian.co.nz>
2023-08-05 23:40:59 +1200

    Put everything into methods. Also runs at start

--- app/script/stats.js
+++ app/script/stats.js
@@ -1,16 +1,32 @@
-window.addEventListener("keydown", () => {
 
-	// Update the current caret position
-	document.querySelector("#rowsAndColumns").textContent = `Row: ${caretY + 1}, Col: ${caretX + 1}`;
+// Run everything initially when it loads and when a key is pressed
+document.addEventListener("DOMContentLoaded", () => update());
+window.addEventListener("keydown", () => update());
+
+function update() {
 	
-	// Word count
-	{
-		// Get the count of all real words
-		const words = output.join(" ").match(/[\w\d\'\'-]+/gi);
-		const wordCount = words ? words.length : 0;
-		let wordCountText = "words";
-		if (wordCount === 1) wordCountText = "word";
-		document.querySelector("#wordCount").textContent = `${wordCount} ${wordCountText}`;
-	}
+	RowsAndColumns();
+	wordCount();
+}
 
-});
+
+
+// Get the current cursor row and column
+function RowsAndColumns() {
+
+	// Update the current caret position text
+	document.querySelector("#rowsAndColumns").textContent = `Row: ${caretY + 1}, Col: ${caretX + 1}`;
+}
+
+// Get how many words there are
+function wordCount() {
+
+	// Get the count of all real words
+	const words = output.join(" ").match(/[\w\d\'\'-]+/gi);
+	const wordCount = words ? words.length : 0;
+
+	// Make it look fancy in the DOM
+	let wordCountText = "words";
+	if (wordCount === 1) wordCountText = "word";
+	document.querySelector("#wordCount").textContent = `${wordCount} ${wordCountText}`;
+}
