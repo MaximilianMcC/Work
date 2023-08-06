dade1fbd4e583af2dca1d1ad8d9438f8e9621ae1
Max <max@maximilian.co.nz>
2023-08-06 20:36:55 +1200

    Added `Insert` method

--- app/script/utils.js
+++ app/script/utils.js
@@ -3,3 +3,8 @@ function clamp(value, min, max) {
 
 	return Math.max(min, Math.min(value, max));
 }
+
+// Insert string into another string at an index
+function insert(originalString, insertedString, index) {
+	return originalString.slice(0, index) + insertedString + originalString.slice(index);
+}
